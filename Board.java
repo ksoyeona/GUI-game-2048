9c50022a6cd4e56078b05b25abd86f8f2ea1e297
ksoyeona <37919264+ksoyeona@users.noreply.github.com>
2020-05-01 07:47:20 +0900

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,471 @@
+// Soyeon Kim, cs8bwaha, 1/27/18
+
+/**
+ * Sample Board
+ * <p/>
+ * 0   1   2   3
+ * 0   -   -   -   -
+ * 1   -   -   -   -
+ * 2   -   -   -   -
+ * 3   -   -   -   -
+ * <p/>
+ * The sample board shows the index values for the columns and rows
+ * Remember that you access a 2D array by first specifying the row
+ * and then the column: grid[row][column]
+ */
+
+import java.util.*;
+import java.io.*;
+
+//this Board class creates a board object and contains methods to save, load,
+//and modify such objects to obtain intended outcome
+public class Board {
+	public final int NUM_START_TILES = 2; 
+	public final int TWO_PROBABILITY = 90;
+	public final int GRID_SIZE;
+
+	private final Random random; // a reference to the Random object, passed in 
+	// as a parameter in Boards constructors
+	private int[][] grid;  // a 2D int array, its size being boardSize*boardSize
+	private int score;     // the current score, incremented as tiles merge 
+
+	// Constructs a fresh board with random tiles
+	public Board(Random random, int boardSize) {
+		this.random = random; 
+		GRID_SIZE = boardSize;
+		grid = new int[GRID_SIZE][GRID_SIZE];
+		addRandomTile();
+		addRandomTile();              
+	}
+
+	// Construct a board based off of an input file
+	// assume board is valid
+	public Board(Random random, String inputBoard) throws IOException {
+		this.random = random; 
+
+		File myFile = new File(inputBoard);
+		Scanner myScanner = new Scanner(myFile);
+
+		//set the first number as grid size
+		GRID_SIZE = myScanner.nextInt();
+
+		//set the second number as score
+		score = myScanner.nextInt();
+
+		//create grid to fill in with tiles
+		grid = new int[GRID_SIZE][GRID_SIZE];        
+
+		while(myScanner.hasNextInt()){
+
+			//loop through the array to fill the grid
+			for(int i = 0; i < grid.length; i++){
+				for(int j = 0; j < grid[i].length; j++){
+					int numb = myScanner.nextInt();
+					grid[i][j] = numb;
+				}
+			}
+
+		}
+	}	
+
+	// Saves the current board to a file
+	public void saveBoard(String outputBoard) throws IOException {
+
+		PrintWriter myWriter = new PrintWriter(outputBoard);
+
+		myWriter.print(GRID_SIZE);
+		myWriter.println();
+		myWriter.print(score);
+		myWriter.println();
+
+		//to append after each number
+		String c = " ";
+		//to start a new line according to grid dimension
+		int incCheck = 0;
+
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid[i].length; j++){
+
+				myWriter.print(grid[i][j]);
+				myWriter.print(c);
+				incCheck++;
+				//start new line after each row
+				if(incCheck % GRID_SIZE == 0)
+				{myWriter.println();}
+
+			}
+		}
+		myWriter.close();
+	}
+
+
+	// Adds a random tile (of value 2 or 4) to a
+	// random empty space on the board
+	public void addRandomTile() {
+
+		int count = 0;
+		//counting number of empty tiles
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid[i].length; j++){
+
+				if(grid[i][j] == 0){
+					count++;
+				}
+			}
+		}
+
+		if(count == 0){
+			return;}
+
+
+		int location = random.nextInt(count);
+		int value = random.nextInt(100);
+
+		//index to find location-th tile. it starts at -1 because
+		//when there's an empty spot then it should increase to 0,
+		//which equals to one tile.
+		int index  = -1;
+
+		for(int i = 0; i < grid.length; i++){
+			for(int j =0; j < grid.length;j++){
+				//	for(int i = 0; i < grid.length; i++){
+				//increase index everytime there's no tile
+				if(grid[i][j] == 0){
+					index++;
+
+					//insert tile when index matches location
+					if(index == location){
+
+						if(value < TWO_PROBABILITY)
+						{grid[i][j] =  NUM_START_TILES;}
+
+						else grid[i][j] = 4;
+					}
+				}
+			}
+			}
+		}
+
+		//helper method to determine whether tiles can move left, and it 
+		//will be used in canMove() method
+		private boolean canMoveLeft(){
+
+			for(int i = 0; i < grid.length; i++){
+				for(int j=1; j < grid[i].length; j++){
+					if(grid[i][j] != 0){
+						if(grid[i][j] == grid[i][j-1] || grid[i][j-1] ==0)
+						{return true;}
+					}
+				}
+			}
+			return false;
+		}
+		//helper method to determine whether tiles can move right, and it 
+		//will be used in canMove() method
+		private boolean canMoveRight(){
+
+			for(int i =0; i < grid.length; i++){
+				for(int j = 0; j < grid[i].length-1;j++){
+					if(grid[i][j] != 0){
+						if(grid[i][j] == grid[i][j+1] || grid[i][j+1]==0)
+						{ return true;}
+					}
+				}
+			}
+			return false;
+		}
+		//helper method to determine whether tiles can move up, and it 
+		//will be used in canMove() method
+		private boolean canMoveUp(){
+
+			for(int i=1; i < grid.length; i++){
+				for(int j =0; j < grid[i].length;j++){
+					if(grid[i][j] != 0){
+						if(grid[i][j] == grid[i-1][j] || grid[i-1][j] == 0)
+						{ return true;}
+					}
+				}
+			}
+			return false;
+		}
+		//helper method to determine whether tiles can move left, and it 
+		//will be used in canMove() method
+		private boolean canMoveDown(){
+
+			for(int i=0; i < grid.length-1; i++){
+				for(int j=0; j < grid[i].length;j++){
+					if(grid[i][j] != 0){
+						if(grid[i][j] == grid[i+1][j] || grid[i+1][j] == 0)
+						{ return true;}
+					}
+				}
+			}
+			return false;
+		}
+		// determins whether the board can move in a certain direction
+		// return true if such a move is possible
+		public boolean canMove(Direction direction){
+
+			for(int i = 0; i < grid.length; i++){
+				for(int j = 0; j < grid[i].length; j++){
+
+					if(direction == Direction.RIGHT && canMoveRight() == true)
+					{return true; }
+					else if(direction == Direction.LEFT && canMoveLeft() == true)
+					{return true; }
+					else if(direction == Direction.UP && canMoveUp() == true)
+					{return true; }
+					else if(direction == Direction.DOWN && canMoveDown() == true)
+					{return true; }
+				}
+			}
+			return false;
+		}
+
+		//helper method for move
+		private void moveLeft(){
+			int x = 0;
+			while( x < grid.length-1 ){
+				for(int i = 0; i < grid.length; i++){
+					for(int j =0; j < grid.length-1; j++){
+
+						//if the tile is not zero and has zero right to it
+						if(grid[i][j+1] != 0 && grid[i][j] ==0){
+
+							//replace the the left spot with tile at location i,j
+							grid[i][j] = grid[i][j+1];
+
+							grid[i][j+1] = 0;
+						}
+					}
+				}x++;//repeats until or the zeros in between are gone
+			}//end of while loop
+
+			for(int i = 0; i < grid.length; i++){
+				for(int j = 1; j < grid.length; j++){
+
+					//if the left tile has same number, combine
+					if(grid[i][j] != 0 && grid[i][j-1] == grid[i][j]){
+
+						grid[i][j-1] = 2*grid[i][j];
+						grid[i][j] = 0;
+						//update score
+						score = score + grid[i][j-1];
+					}
+				}
+			}//end of for loop
+			//get rid of zeros in between just like the first method
+			int b= 0;
+			while(b<grid.length-1){
+				for(int i = 0; i < grid.length; i++){
+					for(int j = 0 ; j < grid.length-1 ; j++){
+						if(grid[i][j+1] != 0 && grid[i][j] ==0){
+							grid[i][j] = grid[i][j+1];
+
+							grid[i][j+1] = 0;
+
+						}
+					}
+				}b++;
+			}
+		}
+		//helper method for move
+		private void moveRight(){
+			int k = 0;
+			while( k < grid.length){
+				for(int i = 0; i < grid.length; i++){
+					for(int j =0; j< grid.length-1; j++){
+
+						//if the tile on the right is zero
+						if(grid[i][j] != 0 && grid[i][j+1] == 0){
+
+							//set that tile as as the number on
+							//ijth location
+							grid[i][j+1] = grid[i][j];
+							grid[i][j] = 0;
+						}
+					}
+				}k++;
+			}//end of while loop		
+
+			//this for loop is for combining
+			for(int i = 0; i < grid.length; i++){
+				for(int j=grid.length-1; j >= 1; j--){
+
+					if(grid[i][j] != 0 && grid[i][j-1] ==grid[i][j]){
+						grid[i][j] = grid[i][j-1] + grid[i][j];
+						grid[i][j-1] = 0;
+						score = score + grid[i][j];
+					}
+				}
+			}//end of for loop
+
+			int h = 0;
+			while(h<grid.length-1){ 	
+				for(int i = 0; i < grid.length; i++){
+					for(int j =0; j<grid.length-1; j++){
+
+						if(grid[i][j] != 0 && grid[i][j+1] == 0){
+							grid[i][j+1] = grid[i][j];
+							grid[i][j] = 0;
+						}
+
+					}
+				}h++;
+			}//end of while loop
+		}
+
+
+		//helper method
+		private void moveUp(){
+			int p=0;
+			while( p < grid.length-1 ){
+
+				for(int j = 0; j < grid.length; j++){
+					for(int i = 1; i < grid.length; i++){
+
+						//if ij tile is non zero and 
+						//tile above is zero set the tile above as 
+						//tile at ij 
+						if(grid[i][j] != 0 && grid[i-1][j] == 0){
+							grid[i-1][j] = grid[i][j];
+							grid[i][j] = 0;
+						}
+					}
+				}
+				p++;
+			}//end of while loop
+
+			for(int j = 0; j < grid.length; j++){
+				for(int i = 1; i < grid.length; i++){
+
+					//if tile is non zero and tile above has 
+					//same number, combine
+					if(grid[i][j]!= 0 && grid[i-1][j] ==grid[i][j]){
+						grid[i-1][j] = grid[i][j] + grid[i][j];
+						grid[i][j] = 0;
+						score = score + grid[i-1][j];
+					}
+				}
+			}//end of for loop
+			int u = 0;
+			while(u < grid.length-1){
+				for(int j = 0; j < grid.length; j++){
+
+					for(int i = 1; i < grid.length; i++){
+
+						if(grid[i][j] != 0 && grid[i-1][j] == 0){
+							grid[i-1][j] = grid[i][j];
+							grid[i][j] =0;
+
+						}
+					}
+				}u++;
+
+			}//end of while loop
+		}
+
+		//helper method
+		private void moveDown(){
+			int z=0;
+
+			while( z< grid.length-1 ){
+
+				for(int j = 0; j < grid.length; j++){
+					for(int i = grid.length-2; i >= 0; i--){
+
+						//if tile at i,j is non zero and tile below is zero, set the bottom 
+						//tile as the value at i,j 
+						if(grid[i][j] != 0 && grid[i+1][j] == 0){
+
+							grid[i+1][j] = grid[i][j];
+							grid[i][j] = 0;
+						}
+					}
+				}
+				z++;
+			}//end of while loop - tiles aligned with no zeros in between
+
+			for(int i = grid.length-1; i > 0; i--){
+				for(int j = 0; j < grid.length; j++){
+
+					//combine tiles if the tile above has same number
+					if(grid[i][j]!= 0 && grid[i][j] ==grid[i-1][j]){
+
+						grid[i][j] = grid[i-1][j] + grid[i][j];
+
+						grid[i-1][j] = 0;
+						//update score
+						score = score + grid[i][j];
+					}
+				}
+			}
+			int s=0;
+			while(s<grid.length-1){
+				for(int j = 0; j < grid.length; j++){
+					for(int i = grid.length-2; i >= 0 ; i--){
+
+						if(grid[i][j] != 0 && grid[i+1][j] == 0){
+
+							grid[i+1][j] = grid[i][j];
+
+							grid[i][j] = 0;
+
+						}
+					}
+				} s++;
+			}//end of while loop
+		}
+
+		// move the board in a certain direction
+		// return true if such a move is successful
+		public boolean move(Direction direction) {
+
+
+			if(canMove(direction) == false){return false;}
+
+			if(Direction.LEFT==direction){moveLeft(); }
+			if(Direction.RIGHT == direction){moveRight();}
+			if(Direction.UP == direction){moveUp();}
+			if(Direction.DOWN == direction){moveDown();}
+
+			return true; 
+		}
+
+		// No need to change this for PSA3
+		// Check to see if we have a game over
+		public boolean isGameOver() {
+
+			if(canMove(Direction.RIGHT) == false &&
+					canMove(Direction.LEFT) == false &&
+					canMove(Direction.UP) == false &&
+					canMove(Direction.DOWN) == false)
+			{return true;}	
+
+			return false;
+		}
+
+		// Return the reference to the 2048 Grid
+		public int[][] getGrid() {
+			return grid;
+		}
+
+		// Return the score
+		public int getScore() {
+			return score;
+		}
+
+		@Override
+			public String toString() {
+				StringBuilder outputString = new StringBuilder();
+				outputString.append(String.format("Score: %d\n", score));
+				for (int row = 0; row < GRID_SIZE; row++) {
+					for (int column = 0; column < GRID_SIZE; column++)
+						outputString.append(grid[row][column] == 0 ? "    -" :
+								String.format("%5d", grid[row][column]));
+					outputString.append("\n");
+				}
+				return outputString.toString();
+
+			}
+	}
---
+++ GameManager.java
@@ -0,0 +1,185 @@
+// Soyeon Kim, cs8bwaha, 2/5/18
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;//for thread
+//this class creates a method that constructs the overall flow 
+//of the game with calling appropiate methods from Board.java
+public class GameManager {
+	// Instance variables
+	private Board board; // The actual 2048 board
+	private String outputBoard; // File to save the board to when exiting
+
+	/*ec*/
+	private String outputRecord; // file to save the record file, format: [size] wasdwasdwasdawsd
+	StringBuilder history = new StringBuilder(); // a string of commands history
+	/*ce*/
+
+	//to implement recording
+	public GameManager(Random random, int boardSize, String outputRecord)throws IOException{
+
+		this.outputRecord = outputRecord;
+		board = new Board(random, boardSize);
+
+		String c = " ";
+
+		File myFile = new File(outputRecord);
+		PrintWriter myWriter = new PrintWriter(myFile);
+
+		myWriter.print(boardSize);
+		myWriter.print(c);
+		myWriter.close();
+
+	}
+
+	//to implement replaying
+	public GameManager(Random random, String inputRecord) throws Exception{
+
+
+		File myFile = new File(inputRecord);
+		Scanner myScanner = new Scanner(myFile);
+		char z; 
+
+		int boardSize = myScanner.nextInt();
+		board = new Board(random, boardSize);
+
+		String string = myScanner.next();
+		int i = 0;
+		while(i < string.length()){
+
+			z= string.charAt(i);
+
+			if(z=='w'){
+				board.move(Direction.UP);}
+
+
+			else if(z=='s'){
+
+				board.move(Direction.DOWN);}
+
+			else if(z=='a'){
+
+				board.move(Direction.LEFT); }
+
+			else if(z=='d'){
+
+				board.move(Direction.RIGHT); }
+
+			Thread.sleep(500);
+			i++;
+		}//end of while loop
+
+	}
+	// TODO PSA3
+	// GameManager Constructor
+	// Generate new game
+	public GameManager(String outputBoard, int boardSize, Random random) throws IOException {
+
+		this.outputBoard = outputBoard;
+		board = new Board(random, boardSize);
+		board.saveBoard(outputBoard);
+	}
+
+	// TODO PSA3
+	// GameManager Constructor
+	// Load a saved game
+	public GameManager(String inputBoard, String outputBoard, Random random) throws IOException {
+
+		this.outputBoard = outputBoard;
+		board = new Board(random, inputBoard);
+		board.saveBoard(outputBoard);
+	}
+
+
+	// TODO PSA3
+	// Main play loop
+	// Takes in input from the user to specify moves to execute
+	// valid moves are:
+	//      w - Move up
+	//      s - Move Down
+	//      a - Move Left
+	//      d - Move Right
+	//      q - Quit and Save Board
+	//
+	//  If an invalid command is received then print the controls
+	//  to remind the user of the valid moves.
+	//
+	//  Once the player decides to quit or the game is over,
+	//  save the game board to a file based on the outputBoard
+	//  string that was set in the constructor and then return
+	//
+	//  If the game is over print "Game Over!" to the terminal
+	public void play() throws Exception {
+
+		// File file = new File(outputRecord);
+		//	PrintWriter writer = new PrintWriter(file);
+
+		printControls();
+
+		System.out.println(board);
+
+		Scanner scanInput = new Scanner(System.in);
+		String str;
+
+		//read user inputs
+		while(scanInput.hasNext()){
+			str = scanInput.next();
+
+			//print "Game over!" when no more movement can be made  
+			if(board.isGameOver()){
+				System.out.println(" Game Over! ");
+				board.saveBoard(outputBoard);
+				return;
+			}
+
+			if(str.equals("w")){
+
+				//			writer.append("w");
+
+				if(board.move(Direction.UP) ==true){board.addRandomTile();}
+				System.out.println(board);}		
+
+			else if(str.equals("s")){
+
+				//			writer.append("s");
+
+				if(board.move(Direction.DOWN)==true){board.addRandomTile();}
+				System.out.println(board);}
+
+			else if(str.equals("a")){
+				//			writer.append("a");
+
+				if(board.move(Direction.LEFT)==true){board.addRandomTile();}
+				System.out.println(board);
+			}
+
+			else if(str.equals("d")){
+				//			writer.append("d");
+
+				if(board.move(Direction.RIGHT)==true){board.addRandomTile();}
+				System.out.println(board);
+			}
+
+			else if(str.equals("q")){
+
+				board.saveBoard(outputBoard);
+				return;
+			}
+
+			else if (str !="w" && str !="s" &&  str !="a" && str !="d" && str !="q")
+			{printControls();}
+		}//end of while loop
+		//writer.close();
+	}
+
+	// Print the Controls for the Game
+	private void printControls() {
+		System.out.println("  Controls:");
+		System.out.println("    w - Move Up");
+		System.out.println("    s - Move Down");
+		System.out.println("    a - Move Left");
+		System.out.println("    d - Move Right");
+		System.out.println("    q - Quit and Save Board");
+		System.out.println();
+	}
+}
